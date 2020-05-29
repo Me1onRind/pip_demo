edc6fef6da0676bbd11c5fe34f701519543346e7
Me1onRind <320314319@qq.com>
2020-05-30 04:31:33 +0800

    init test

---
+++ foo/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+	name = "bar",
+	version = "0.0.1",
+	description = "Just demo",
+	licence = "Apache License Version 2.0",
+	author = "Me1onRind",
+
+	packages = find_packages(),
+	include_package_data = True,
+	install_requires = [],
+)
